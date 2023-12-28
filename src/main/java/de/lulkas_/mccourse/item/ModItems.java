d138c0a999d126beb80067be83f3b392a7884180
LukasGP1 <lukas.hilger@icloud.com>
2023-12-28 08:46:05 +0100

    added the raw alexandrite item

--- src/main/java/de/lulkas_/mccourse/item/ModItems.java
+++ src/main/java/de/lulkas_/mccourse/item/ModItems.java
@@ -14,6 +14,10 @@ public class ModItems {
             () -> new Item(new Item.Properties())
     );
 
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties())
+    );
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
